baee6067a7dd50538e8d4ea3ee6b71713682343e
Jan Bajor <jan_bajor@wp.pl>
2026-06-05 19:47:52 +0200

    Zmiana wagi

--- ProjektZaliczeniowy/edge.hpp
+++ ProjektZaliczeniowy/edge.hpp
@@ -10,7 +10,7 @@ public:
     T source;
     T target;
     float weight;
-    Edge(T s, T t, float w=1.0) : source(s), target(t), weight(w) {}
+    Edge(T s, T t, float w) : source(s), target(t), weight(w) {}
     ~Edge() {}                // destruktor
     Edge(const Edge& edge) : source(edge.source), target(edge.target),
         weight(edge.weight) {}  // copy-constructor
